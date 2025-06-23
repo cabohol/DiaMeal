92c1801d5d299ad323c40fbf647c3bd5c1a94454
kleyr567 <claireannebohol567@gmail.com>
2025-06-23 16:32:57 +0800

    added get started and login page

--- DiaMeal-app/src/router/index.js
+++ DiaMeal-app/src/router/index.js
@@ -1,23 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Advertise from '@/views/auth/Advertise.vue'
+import LoginView from '@/views/auth/LoginView.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/advertise' // ✅ redirect only, no component here
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/advertise',
+      component: Advertise // ✅ this is where you mount your component
+    },
+     {
+      path: '/LoginView',
+      name: 'login',
+      component: LoginView
     },
   ],
 })
+
 
 export default router

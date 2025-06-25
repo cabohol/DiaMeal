9e03b06bf6bae8f3aab7c66bc8d636087949300e
kleyr567 <claireannebohol567@gmail.com>
2025-06-25 23:58:08 +0800

    added my progress and fixed full name

--- DiaMeal-app/src/router/index.js
+++ DiaMeal-app/src/router/index.js
@@ -6,6 +6,7 @@ import RegisterView from '@/views/auth/RegisterView.vue'
 import TermsAndConditions from '@/views/TermsAndConditions.vue'
 import ProfileView from '@/views/auth/ProfileView.vue'
 import MealPlan from '@/views/auth/MealPlan.vue' 
+import MyProgress from '@/views/auth/MyProgress.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -47,6 +48,11 @@ const router = createRouter({
       path: '/meal-plan', 
       name: 'meal-plan',
       component: MealPlan
+    },
+    {
+      path: '/myprogress', 
+      name: 'myprogress',
+      component: MyProgress
     },
   ],
 })
